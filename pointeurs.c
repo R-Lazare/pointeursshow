97e9b366dc7a3a47ac30b10571c9ce1d47c73538
rluiz <rluiz@ubuntu.myguest.virtualbox.org>
2023-03-06 23:10:20 +0100

    2

--- pointeurs.c
+++ pointeurs.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 
+void	afficher_a(int *a);
+
 int	main(void)
 {
 	int a;
 
 	a = 42;
-	printf("Hello World !");
+	printf("pointeur sur a ( &a ) = %p \n", &a);	
+	printf("valeur de a ( a )  = %d \n", a);
+	afficher_a(&a);
 	return (0);	
+}
+
+void	afficher_a(int *b)
+{
+	printf("pointeur sur b ( b ) = %p \n", b);
+	printf("valeur de b ( *b ) = %d \n", *b);
 }
