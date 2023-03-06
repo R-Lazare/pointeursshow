59dfbe4a1d31243715febeb33ea2ad718b6f8336
rluiz <rluiz@ubuntu.myguest.virtualbox.org>
2023-03-06 23:45:05 +0100

    presentation done

--- pointeurs.c
+++ pointeurs.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
 void	afficher_a(int *a);
+void	afficher_tab(int *tab, int len);
 
 int	main(void)
 {
 	int a;
 
 	a = 42;
-	printf("pointeur sur a ( &a ) = %p \n\n", &a);	
-	printf("valeur de a ( a )  = %d \n\n", a);
+	printf("pointeur sur a ( &a ) = %p \n", &a);	
+	printf("valeur de a ( a )  = %d \n", a);
 	afficher_a(&a);
 
 	printf("------------------------------------------------------------\n");
@@ -20,9 +21,10 @@ int	main(void)
 	tab[2] = 3;
 	tab[3] = 4;
 
-	printf("adresse de tab[0] : %p\n\nadresse de tab[1] : %p\n\nvaleur tab[0] = %d\n\nvaleur tab[1] : *(tab + 1) = %d \n", &tab, (tab + 1), tab[0], *(tab + 1) );
+	printf("adresse de tab[0] (tab) = %p\nadresse de tab[1] (tab + 1) = %p\nvaleur tab[0] (tab[0]) = %d\nvaleur tab[1] : *(tab + 1) = %d \n", tab, (tab + 1), tab[0], *(tab + 1) );
 
 
+	afficher_tab(tab, 4);
 	return (0);	
 }
 
@@ -30,6 +32,27 @@ int	main(void)
 
 void	afficher_a(int *b)
 {
-	printf("pointeur sur b ( b ) = %p \n\n", b);
-	printf("valeur de b ( *b ) = %d \n\n", *b);
+	printf("pointeur sur b ( b ) = %p \n", b);
+	printf("valeur de b ( *b ) = %d \n", *b);
 }
+
+void	afficher_tab(int *tab, int len)
+{
+	int	i;
+	
+	printf("tab[0] (*tab) = %d\n\n", *tab);
+	i = 0;
+	while (i < len)
+	{
+		printf("tab[%d] = %d\n", i, tab[i]);
+		i++;
+	}
+
+	i = 0;
+	while (i < len)
+	{
+		printf("tab[%d] = %d\n", i, *(tab + i));
+		i++;
+	}
+}
+
