c08ff4817b99394c8a39a6a59693ed9ce5f78704
rluiz <rluiz@ubuntu.myguest.virtualbox.org>
2023-03-06 23:30:04 +0100

    3

--- pointeurs.c
+++ pointeurs.c
@@ -7,14 +7,29 @@ int	main(void)
 	int a;
 
 	a = 42;
-	printf("pointeur sur a ( &a ) = %p \n", &a);	
-	printf("valeur de a ( a )  = %d \n", a);
+	printf("pointeur sur a ( &a ) = %p \n\n", &a);	
+	printf("valeur de a ( a )  = %d \n\n", a);
 	afficher_a(&a);
+
+	printf("------------------------------------------------------------\n");
+
+	int tab[4];
+	
+	tab[0] = 1;
+	tab[1] = 2;
+	tab[2] = 3;
+	tab[3] = 4;
+
+	printf("adresse de tab[0] : %p\n\nadresse de tab[1] : %p\n\nvaleur tab[0] = %d\n\nvaleur tab[1] : *(tab + 1) = %d \n", &tab, (tab + 1), tab[0], *(tab + 1) );
+
+
 	return (0);	
 }
 
+
+
 void	afficher_a(int *b)
 {
-	printf("pointeur sur b ( b ) = %p \n", b);
-	printf("valeur de b ( *b ) = %d \n", *b);
+	printf("pointeur sur b ( b ) = %p \n\n", b);
+	printf("valeur de b ( *b ) = %d \n\n", *b);
 }
